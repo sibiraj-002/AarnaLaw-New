b44e95b1e28925f912cbdec404ecc4357ee7b093
sibiraj-002 <shansibi2002@gmail.com>
2024-09-16 15:56:05 +0530

    banner text and image updated

--- components/HomeComponent/HomeBanner.js
+++ components/HomeComponent/HomeBanner.js
@@ -6,16 +6,15 @@ import "slick-carousel/slick/slick-theme.css";
 import Navbar from "@components/Navbar";
 
 function HomeBanner() {
-  var setting = {
+  var settings = {
     speed: 500,
     slidesToShow: 1,
     initialSlide: 1,
     slidesToScroll: 1,
     fade: false,
     autoplay: true,
-    arrow: false,
+    arrows: false,
     dots: false,
-
     responsive: [
       {
         breakpoint: 1024,
@@ -36,21 +35,20 @@ function HomeBanner() {
   };
 
   return (
-    <section className="text-center relative w-full mx-auto overflow-hidden md:h-screen">
+    <section className="relative w-full mx-auto overflow-hidden md:h-screen text-center">
       <Navbar />
-      <div className="text-center">
-        <BannerSlider {...setting} className="z-0">
+      <div className="relative">
+        <BannerSlider {...settings} className="z-0">
           <div className="relative overflow-hidden">
             <img
               src="/home-page/home-banner-1.jpg"
-              className="w-full lg:h-auto h-[500px] z-0"
+              className="w-full h-[400px] lg:h-auto z-0"
               width={800}
               height={150}
-              alt=""
-              priority
+              alt="Home Banner 1"
             />
-            <div className="absolute inset-0 flex items-center justify-center text-center z-50">
-              <p className="text-white lg:text-6xl text-3xl font-bold">
+            <div className="absolute inset-0 flex items-end md:items-center justify-center text-center z-10 pb-8 md:pb-0">
+              <p className="text-white font-bold text-3xl lg:text-6xl">
                 Unlocking Abundance
               </p>
             </div>
@@ -59,44 +57,40 @@ function HomeBanner() {
           <div className="relative overflow-hidden">
             <img
               src="/home-page/home-banner-3.jpg"
-              className="w-full lg:h-auto h-[500px] z-0"
+              className="w-full h-[400px] lg:h-auto z-0"
               width={800}
               height={150}
-              alt=""
-              priority
+              alt="Home Banner 3"
             />
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-50 lg:pt-0 pt-32">
-              <p className="text-white lg:text-6xl text-3xl font-bold">
+            <div className="absolute inset-0 flex flex-col items-end md:items-center justify-end md:justify-center text-center z-10 pb-8 md:pb-0">
+              <p className="text-white font-bold text-3xl lg:text-6xl">
                 Client-Centric Problem Solving
               </p>
-              <p className="text-white lg:text-2xl text-1xl mt-4">
+              <p className="mt-4 text-white text-1xl lg:text-2xl">
                 We are a counsel-led Indian legal advisory firm, representing
                 India and people across the world in diverse areas of expertise.
               </p>
-              {/* <button className="text-red-600 bg-white hover:bg-red-700 hover:text-white p-2 mt-8 rounded-none">
-                Contact Us
-              </button> */}
             </div>
           </div>
 
           <div className="relative overflow-hidden">
-            <img
-              src="/home-page/home-banner-2.jpg"
-              className="w-full lg:h-auto h-[500px] z-0"
-              width={800}
-              height={150}
-              alt=""
-              priority
-            />
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-center z-50">
-              <p className="text-white lg:text-6xl text-3xl font-bold">
-                Rooted in India,
-              </p>
-              <p className="mt-4 text-white lg:text-6xl text-3xl font-bold">
-                Global in Practice.
-              </p>
-            </div>
-          </div>
+  <img
+    src="/home-page/home-banner-2.jpg"
+    className="w-full h-[400px] lg:h-auto z-0"
+    width={800}
+    height={150}
+    alt="Home Banner 2"
+  />
+  <div className="absolute inset-0 flex flex-col items-center justify-end text-center z-10 pb-8 md:pb-0 md:justify-center">
+    <p className="text-white font-bold text-3xl lg:text-6xl">
+      Rooted in India,
+    </p>
+    <p className="mt-4 text-white font-bold text-3xl lg:text-6xl">
+      Global in Practice.
+    </p>
+  </div>
+</div>
+
         </BannerSlider>
       </div>
     </section>
